103351ee6ea0f3ab3ead0b9f1433345bb982fe2b
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-13 04:24:42 -0700

    uploaded project

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 song = '';
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 
 function preload() {
     song = loadSound("music.mp3");
@@ -9,6 +15,8 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
@@ -17,4 +25,27 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.X;
+        leftWristY = results[0].pose.leftWrist.Y;
+
+        rightWristX = results[0].pose.rightWrist.X;
+        rightWristY = results[0].pose.rightWrist.Y;
+
+        console.log("Left Wrist X = " + leftWristX);
+        console.log("Left Wrist Y = " + leftWristY);
+
+        console.log("Right Wrist X = " + rightWristX);
+        console.log("Right Wrist Y = " + rightWristY);
+    }
 }
