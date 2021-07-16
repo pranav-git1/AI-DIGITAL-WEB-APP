8cbee0aab1be7cffa6ec025296f6c51635193194
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-16 04:25:56 -0700

    uploaded

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
+song = '';
+scoreLeftWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
+
+scoreRightWrist = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+
 function preload() {
-    first = loadSound("first.mp3");
-    second = loadSound("2nd.mp3");
+    song = loadSound("music.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(600, 400);
+    canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -18,21 +22,62 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 600, 400);
+    image(video, 0, 0, 600, 500);
+    fill("#ff0000");
+    stroke("#ff0000");
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberLeftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberLeftWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("Volume").innerHTML = volume;
+        song.setVolume(volume);
+    }
+    circle(rightWristX, rightWristY, 20);
+    if (rightWristY > 0 && rightWristY <= 100) {
+        document.getElementById("Speed").innerHTML = "0.5x";
+        song.rate(0.5);
+    } else if (rightWristY > 100 && rightWristY <= 200) {
+        document.getElementById("Speed").innerHTML = "1x";
+        song.rate(1);
+    } else if (rightWristY > 200 && rightWristY <= 300) {
+        document.getElementById("Speed").innerHTML = "1.5x";
+        song.rate(1.5);
+    } else if (rightWristY > 300 && rightWristY <= 400) {
+        document.getElementById("Speed").innerHTML = "2x";
+        song.rate(2);
+    } else if (rightWristY > 400 && rightWristY <= 500) {
+        document.getElementById("Speed").innerHTML = "2.5x";
+        song.rate(2.5);
+    }
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function modelLoaded() {
-    console.log('PoseNet is initialized');
+    console.log('PoseNet is Initialized');
 }
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score Left Wrist = " + scoreLeftWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
 
-        leftWristX = results[0].pose.X;
-        leftWristY = results[0].pose.Y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
 
-        rightWristX = results[0].pose.X;
-        rightWristY = results[0].pose.Y;
+        console.log("Left Wrist X = " + leftWristX);
+        console.log("Left Wrist Y = " + leftWristY);
+
+        console.log("Right Wrist X = " + rightWristX);
+        console.log("Right Wrist Y = " + rightWristY);
     }
 }
